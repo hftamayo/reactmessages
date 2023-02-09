291f08f2181ed7599813c4ea040e12fe3930327e
hftamayo <hftamayo@gmail.com>
2023-02-09 10:50:11 -0600

    sync method for refresh the DOM sync

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -44,6 +44,20 @@ class Feed extends Component {
     openSocket('http://localhost:3006');
   }
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if(prevState.postPage === 1){
+        updatedPosts.pop();
+        updatedPosts.unshift(post);
+      }
+      return{
+        post: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    })
+  }
+
   loadPosts = (direction) => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
